45712f4d0883151c97730d3fe0c0a6c21fa92876
Andrey Drobnyi <drobnyy2001@gmail.com>
2022-06-21 15:49:58 +0300

    improvements

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 let burger = document.querySelector(".burger");
 let navbarToggler = document.querySelector(".navbar-toggler");
 let navbarCollapse = document.querySelector(".navbar-collapse");
+let salePrice = document.querySelectorAll(".sale-price");
+let cardItem = document.querySelectorAll(".card-item");
+let mainPrice = document.querySelectorAll(".main-price");
+
+
 
 navbarToggler.addEventListener("click", () => {
   burger.classList.toggle("trans");
@@ -15,3 +20,20 @@ $('.accessories').flickity({
   wrapAround: true,
   cellAlign: 'left'
 });
+
+
+function sale() {
+  for (let i = 0; i < salePrice.length; i++) {
+    if (salePrice[i].innerHTML) {
+      cardItem[i].classList.add("sale");
+      mainPrice[i].classList.add("close-price");
+    } else {
+      cardItem[i].classList.remove("sale");
+      mainPrice[i].classList.remove("close-price");
+    }
+  }
+
+}
+
+
+sale();
